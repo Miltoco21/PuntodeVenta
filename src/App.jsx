cffe32c3358e45f747792fc7eb1133db17ab3f63
MiltonP <milton206@gmail.com>
2023-10-18 12:41:12 -0300

    suma de productos diseno

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home"; 
 import Consolas from "./Pages/Consolas";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
   return (
--- src/Components/BoxOptions/BoxSumaProd.jsx
+++ src/Components/BoxOptions/BoxSumaProd.jsx
@@ -32,7 +32,7 @@ import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
 
 const BoxSumaProd = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
 
   const increment = () => {
     setCount(count + 1);
@@ -41,7 +41,7 @@ const BoxSumaProd = () => {
   const decrement = () => {
     setCount(count - 1);
   };
-  const [plu, setPlu] = useState('');
+  const [plu, setPlu] = useState("");
 
   const handlePluChange = (event) => {
     setPlu(event.target.value);
@@ -49,12 +49,14 @@ const BoxSumaProd = () => {
 
   const handlePluSubmit = () => {
     // You can handle the PLU submission here, e.g., send it to the server.
-    console.log('PLU submitted:', plu);
+    console.log("PLU submitted:", plu);
   };
 
   return (
     <Paper
       elevation={13}
+      item
+      lg={11}
       sx={{
         display: "flex",
         flexDirection: "column",
@@ -63,72 +65,109 @@ const BoxSumaProd = () => {
         marginBottom: "10px",
         marginLeft: "5px",
         marginTop: "-340px",
-        justifyContent: "space-around",
-        alignItems: "center",
+        justifyContent: "center",
+        
       }}
     >
-      <Grid
-        container
-        spacing={1}
-        
-      >
-        <Grid item xs={12}>
-          <Paper>
-            {/* Main Sales Display */}
-            <div className="sales-display">
-              {/* You can put your main sales data here */}
-              <p>descripcion - valor - producto</p>
-            </div>
+      <Grid container spacing={1}>
+        <Grid item xs={12} lg={12}>
+          {/* Main Sales Display */}
+          <Paper
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              padding: "21px",
+              margin: "5px",
+            }}
+            elevation={21}
+            className="sales-display"
+          >
+            {count}
           </Paper>
         </Grid>
-        <Grid item xs={4} lg={4} >
-          <Paper elevation={3} sx={{ display: "flex", flexDirection: "row" }}>
-            <Grid className="product-box">
-              <p>Count: {count}</p>
-              
+        <Grid item xs={4} lg={12}>
+          <Paper
+            elevation={14}
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              padding: "5px",
+              margin: "5px",
+              marginTop:"-6px"
+            }}
+          >
+            <Grid container spacing={1}>
+              <Grid item xs={3} lg={1}>
+                <div className="product-box"  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent:"space-around",
+                    alignItems: "center",
+                  }}>
+                  <Typography>{count}</Typography>
+                </div>
+              </Grid>
+              <Grid item xs={3} lg={2}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                  size="small"
+                >
+                  <Button
+                    size="small"
+                    variant="outlined"
+                    aria-label="reduce"
+                    onClick={decrement}
+                  >
+                    <RemoveIcon fontSize="small" />
+                  </Button>
+                  <Button
+                    size="small"
+                    variant="outlined"
+                    aria-label="increase"
+                    onClick={increment}
+                  >
+                    <AddIcon fontSize="small" />
+                  </Button>
+                </div>
+              </Grid>
+              <Grid item fullWidth xs={3} lg={4}>
+                <div className="product-box">
+                  <TextField focused placeholder="Ingrese código" />
+                </div>
+              </Grid>
+              <Grid item xs={3} lg={4}>
+                <div style={{ display: "flex" }} className="product-box">
+                  <TextField placeholder=" PLU..." />
+                  <Button
+                    size="small"
+                    variant="outlined"
+                    onClick={handlePluSubmit}
+                  >
+                    PLU
+                  </Button>
+                </div>
+              </Grid>
             </Grid>
-            <Grid sx={{ display: "flex",flexDirection:"column-reverse"}}>
-              <ButtonGroup size="medium">
-                <Button aria-label="reduce" onClick={decrement}>
-                  <RemoveIcon fontSize="small" />
-                </Button>
-                <Button aria-label="increase" onClick={increment}>
-                  <AddIcon fontSize="small" />
-                </Button>
-              </ButtonGroup>
-            </Grid>
           </Paper>
         </Grid>
-        <Grid item xs={4}>
-          <Paper>
-            {/* Product Box 2 */}
-            <div className="product-box">
-              {/* You can put product information or buttons here */}
-              <Typography></Typography>
-              <Input placeholder="Ingrese código"></Input>
-            </div>
-          </Paper>
-        </Grid>
-        <Grid item xs={4} lg={4}>
-          <Paper sx={{ display: "flex", flexDirection: "row" }}>
-            {/* Product Box 3 */}
-            <div className="product-box">
-        <TextField
-          placeholder="Cod PLU3455"
-          value={plu}
-          onChange={handlePluChange}
-        />
-        <Button
-          variant="outlined"
-          onClick={handlePluSubmit}
-        >
-          PLU
-        </Button>
-      </div>
-          </Paper>
-        </Grid>
+
         <Grid item xs={12}>
-          <Paper>
+          <Paper
+          elevation={14}
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              margin: "5px",
+              marginTop:"-6px"
+            }}
+          >
             {/* Sales Table */}
             <TableContainer>
               <Table>
